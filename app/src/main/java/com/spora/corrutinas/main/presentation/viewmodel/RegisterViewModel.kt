d5ce48809b056ffa478795f71a860087ea82a4fa
yerih 3d link <yerih@3dlinkweb.com>
2022-09-02 17:40:58 -0400

    Avance en register activity.

--- app/src/main/java/com/spora/corrutinas/main/presentation/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/spora/corrutinas/main/presentation/viewmodel/RegisterViewModel.kt
@@ -2,14 +2,42 @@ package com.spora.corrutinas.main.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.spora.corrutinas.databinding.ActivityRegisterBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel(){
+class RegisterViewModel: ViewModel(){
 
-    fun requestData(){
-        viewModelScope.launch(Dispatchers.IO) {
+    data class RegisterData(
+        var name: String = "",
+        var paternal: String = "",
+        var maternal: String = "",
+        var email: String = "",
+        var address: String = "",
+        var phone: String = "",
+        var date: String = "",
+    )
+    private val data = RegisterData()
 
+    private fun checkInput(binding: ActivityRegisterBinding): Boolean{
+        var check = true
+        with(binding){
+            with(data){
+                name = inputName.text.toString().ifEmpty { check = false}
+                paternal = inputPaternal.text.toString().also { check = false }
+                maternal = inputMaternal.text.toString().also { check = false }
+                phone = inputPhone.text.toString().also { check = false }
+                date = inputDate.text.toString().also { check = false }
+                email = inputEmail.text.toString().also { check = false }
+            }
+            layoutName.isErrorEnabled = .isEmpty().also { if(it)check = it }
         }
+        return check
     }
+
+    fun onSendClicked(binding: ActivityRegisterBinding){
+        if(!checkInput(binding))return
+    }
+
+    fun onCancelClicked(){}
 }
